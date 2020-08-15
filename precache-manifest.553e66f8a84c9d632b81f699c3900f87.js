807adf8f9a8a2c7351823c2d079ff3b90f473f3c
apoorvdwi <apoorvd14@gmail.com>
2020-08-15 22:00:28 +0530

    Updates

---
+++ precache-manifest.553e66f8a84c9d632b81f699c3900f87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e06e80377c32cf0816761f25ac56c61e",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "0b1e90e27334848b2d38",
+    "url": "/Keeper/static/js/2.90f3b997.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Keeper/static/js/2.90f3b997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d604154f0505c80fa55b",
+    "url": "/Keeper/static/js/main.b976018b.chunk.js"
+  },
+  {
+    "revision": "e0225f56b4f7c3f6a1e9",
+    "url": "/Keeper/static/js/runtime-main.02927e8d.js"
+  }
+]);
